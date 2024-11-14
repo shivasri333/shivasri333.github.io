2dec419998b6ec0f637f860a94d9ed08b2c0d19c
Ravi Kiran Vadde <vadderavikiran@gmail.com>
2024-11-14 18:20:41 -0500

    code

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Shivasri Kumbam. I'm a Full Stack Developer based in Dallas, Texas.",
+    description1: "My name's Shivasri Kumbam. I'm a Full Stack Developer based in Dallas, TX.",
     description2: "With over 5 years of experience, I specialize in Java, Spring Boot, and microservices architecture, seamlessly integrating front-end frameworks like Angular with RESTful APIs and backend services. My recent Master's from Texas Tech University sharpened my skills in big data processing with Apache Spark, containerization with Docker, and CI/CD practices on cloud platforms like AWS and Azure. In my current role, I contribute to building scalable, resilient applications, optimizing data processing, and mentoring junior developers to enhance overall team performance.",
     image: 2
 }
